28533417818c39c2b0a8325e485a3ebfecd67444
lisasha01 <113851734+lisasha01@users.noreply.github.com>
2022-09-19 00:03:06 +0700

    init

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="резюме, стаж, опыт, образование, личные качества, навыки">
+    <meta name="description" content="Страница резюме Ли А.Е.">
+    <title>Резюме</title>
+</head>
+<body>
+    <img src="images/myface.jpg" alt="Мое лицо" width="100px">
+    <h1>Ли Александр Евгеньевич</h1>
+    <p><strong> Образование:</strong><em> высшее (специалитет + магистратура)</em></p>
+    <p><strong>Опыт:</strong><em> более 10 лет</em></p>
+    <p><strong>Возраст:</strong><em> 33 года</em></p> 
+    <h3>Я в социальных сетях</h3>
+    <a href="https://vk.com/lisasha01" target="_blank"><p>Я в Контакте</p></a>
+    <a href="https://www.avito.ru/seversk/predlozheniya_uslug/remont_kompyuterov_noutbukov_smartfonov_2383474631" target="_blank"><p>Я на Авито</p></a>
+    <h3>Мои увлечения</h3>
+    <ol>
+        <li>Семья</li>
+        <li>Спорт</li>
+        <li>Бизнес</li>
+        <li>Программирование</li>
+        <li>Электроника</li>
+    </ol>
+    <h3>Опыт работы</h3>
+    <table>
+        <thead>
+        <tr>
+            <th>Название</th>
+            <th>Должность</th>
+            <th>Период</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>ОАО "ИНТЕК"</td>
+            <td>Сервис-инженер</td>
+            <td>2008 - 2013</td>
+        </tr>
+        <tr>
+            <td>ОАО "Ростелеком"</td>
+            <td>Инженер базовых станций</td>
+            <td>2013 - 2014</td>
+        </tr>
+        <tr>
+            <td>ООО "ТОМТЕЛ"</td>
+            <td>Инженер электросвязи</td>
+            <td>2014 - 2018</td>
+        </tr>
+        <tr>
+            <td>АО "ЭР-Телеком Холдинг"</td>
+            <td>Сервисный инженер</td>
+            <td>2018 - 2022</td>
+        </tr>
+        </tbody>
+        <tfoot>
+
+        </tfoot>
+    </table>
+    <h3>Связаться со мной</h3>
+    <form action="">
+        <div>
+        <label for="name">Ваше имя</label>
+        <input id="name" type="text" name="name" placeholder="Введите имя">
+        </div>
+        <div>
+        <label for="email">Ваш email</label>
+        <input id="email" type="email" name="email" placeholder="Введите email">
+        </div>
+        <div>
+            <label for="mess">Введите ваше сообщение</label>
+            <br>
+            <textarea name="message" id="mess" cols="30" rows="10" placeholder="Ваше сообщение"></textarea>
+        </div>
+        <button type="submit">Отправить</button>
+    </form>
+    
+</body>
+</html>
